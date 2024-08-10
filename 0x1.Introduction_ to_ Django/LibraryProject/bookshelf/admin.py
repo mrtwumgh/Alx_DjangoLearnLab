eb2c785e154a1daa682942f4502876cb73940c99
mrtwumgh <twumstephen2006@gmail.com>
2024-08-10 07:46:06 +0000

    renamed admin

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Book
 
-class BookAdmin(admin.ModelAmin):
+class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
 
