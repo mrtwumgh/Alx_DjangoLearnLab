0579cd5664636a5cb8b0317ea05db28d685b16d2
mrtwumgh <twumstephen2006@gmail.com>
2024-08-17 09:18:30 +0000

    modifed the views and url app

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView
 from .models import Author, Book
 from .models import Library
 
@@ -10,6 +10,6 @@ def list_books(request):
     }
     return render(request, 'relationship_app/list_books.html', context)
 
-class LibraryDetail(ListView):
+class LibraryDetail(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
