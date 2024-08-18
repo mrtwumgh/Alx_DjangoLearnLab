a0b0a9937cc247ca3863d5aeeba5834ca1e63204
mrtwumgh <twumstephen2006@gmail.com>
2024-08-18 09:29:16 +0000

    setup signals and roles

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -28,3 +28,19 @@ def is_admin(user):
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
