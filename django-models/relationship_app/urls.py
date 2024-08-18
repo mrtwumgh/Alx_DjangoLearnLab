f417000a308822e535841f88058c4e2fa1e8ba26
mrtwumgh <twumstephen2006@gmail.com>
2024-08-18 08:54:07 +0000

    setup signals and roles

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('library_detail/', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html')),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html', name='logout')),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('admin_view/',  views.admin_view, name='admin_view' ),
+    path('librarian_view/', views.librarian_view, name='librarian_view'),
+    path('member_view', views.member_view, name='member_view')
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,6 +4,7 @@ from .models import Book
 from .models import Library
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 def list_books(request):
@@ -19,3 +20,24 @@ class LibraryDetailView(DetailView):
 def register(request):
     form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
