5e952a333870a3b10e168dc70750c2d6c4eb1b9b
mrtwumgh <twumstephen2006@gmail.com>
2024-08-18 11:16:40 +0000

    setup permissions

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,7 +4,8 @@ from .models import Book
 from .models import Library
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 
 # Create your views here.
 def list_books(request):
