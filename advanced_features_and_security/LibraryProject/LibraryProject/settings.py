c756bdb3faf5b99ff9f95e3a499239c8c883169b
mrtwumgh <twumstephen2006@gmail.com>
2024-08-24 15:58:18 +0000

    adjusted the auth user model

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -100,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'bookshelf.MyUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
