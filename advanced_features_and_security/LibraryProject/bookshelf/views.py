ac3b4f9350cbd3a8282ca4a22c0431f8e5591a3f
mrtwumgh <twumstephen2006@gmail.com>
2024-08-25 07:07:12 +0000

    groups added

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,60 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from .models import Book
+from .models import Library
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 
+# Create your views here.
+def list_books(request):
+    context = {
+        'book_list': Book.objects.all()
+    }
+    return render(request, 'relationship_app/list_books.html', context)
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+
+def register(request):
+    form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    return render(request, 'relationship_app/add_book.html')
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request):
+    return render(request, 'relationship_app/edit_book.html')
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request):
+    return render(request, 'relationship_app/delete_book.html')
 # Create your views here.
