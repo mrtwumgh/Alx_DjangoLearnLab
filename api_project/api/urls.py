db430c58b15aebb02cfe88f5c2ee51e8b77fed08
mrtwumgh <twumstephen2006@gmail.com>
2024-09-01 06:38:39 +0000

    configured the ModelViewSet for the Book Model

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from api import views
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+
+router.register(r'books', views.BookViewSet, basename='book')
 
 urlpatterns = [
     path('books/', views.BookList.as_view(), name='books'),
-]
+] + router.url
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from .models import Book
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .serializers import BookSerializer
 
 
+
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
