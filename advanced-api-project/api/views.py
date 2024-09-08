1bcbab51c5960825e06caaa9738f68ccd29fc56b
mrtwumgh <twumstephen2006@gmail.com>
2024-09-08 13:54:46 +0000

    test

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from .serializers import BookSerializer
 from .models import Book
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters import rest_framework
+from rest_framework import generics
 from rest_framework import filters
 
 # Create your views here.
