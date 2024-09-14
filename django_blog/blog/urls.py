a7332e9642955c720041723830b1c724334afa52
mrtwumgh <twumstephen2006@gmail.com>
2024-09-14 16:56:55 +0000

    added bootstrap

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from blog import views
+from blog import views as blog_views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('register/', blog_views.RegisterView.as_view(), name='register'),
+    path('profile/', blog_views.ProfileView.as_view(), name='profile'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import FormView, View
+from blog.forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib import messages
+from django.contrib.auth import logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 # Create your views here.
-def home(request):
-    return render(request, 'blog/base.html')
+class RegisterView(FormView):
+    template_name = 'blog/register.html'
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request, "Your Account has been created successfully")
+        return super().form_valid(form)
+    
+
+
+class ProfileView(LoginRequiredMixin, View):
+    template_name = 'blog/profile.html'
+
+    def get(self, request, *args, **kwargs):
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+        context = {
+            'u_form': u_form,
+            'p_form': p_form,
+        }
+
+        return render(request, self.template_name, context)
+    
+    def post(self, request, *args, **kwargs):
+        u_form = UserUpdateForm(request.POST, request.FILES, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, "Your profile has been updated successfully")
+            return redirect('profile')
+        
+        context = {
+            'u_form': u_form,
+            'p_form': p_form,
+        }
+
+        return render(request, self.template_name, context)
