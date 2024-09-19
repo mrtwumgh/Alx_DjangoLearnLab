9ffdcb31b6ce8f2574f8b25671f131827859c986
mrtwumgh <twumstephen2006@gmail.com>
2024-09-19 08:03:27 +0000

    defined the write_only attribute for the password and password2 fields in the meta class

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from accounts.models import CustomUser
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,12 +13,17 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    password2 = serializers.CharField(write_only=True, label='confirm password')
+    password = serializers.CharField()
+    password2 = serializers.CharField(label='confirm password')
 
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password', 'password2']
+
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'password2': {'write_only': True},
+        }
 
     def validate(self, data):
         if data['password'] != data['password2']:
@@ -27,8 +33,8 @@ class RegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop('password2')
 
-        user = user = get_user_model().objects.create_user(**validated_data)
-        token, created = Token.objects.get_or_create(user=user)
+        user = get_user_model().objects.create_user(**validated_data)
+        token = Token.objects.create(user=user)
         return {
             'user': user,
             'token': token.key
